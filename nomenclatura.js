a095bf86761c19235ea87225c043b8c79384d9ad
annaagabi <annaa0478@gmail.com>
2024-08-17 13:20:28 -0300

    Feat: material aula 17-08

---
+++ nomenclatura.js
@@ -0,0 +1,53 @@
+// Tipos de variáveis
+var a = "Chocolate"
+let b = "Laranja"
+const c = 10  // "const" não pode receber um novo valor
+const d = "Não deu certo"
+
+let obj = {
+    name: "Lucas",
+    age: 20
+}
+
+a = 111
+// c = "teste"
+
+// Tipos de console
+console.log(a)
+console.warn(b)
+console.error(d)
+console.table(obj)
+
+// Limpar console após a visualização
+// console.clear()
+
+// Tipos de dados
+
+// String
+const name = "Texto"
+
+// Number
+const num = 1
+
+// Boolean
+const isApproved = false
+
+// Nulo
+const lastName = null
+
+// Undefined
+let address
+
+// Object
+const brand = {
+    name: "Nike",
+    size:["P", "M", "G"]
+}
+
+// Array
+const languages =[
+    "Java",
+    "JavaScipt"
+]
+
+console.log()
